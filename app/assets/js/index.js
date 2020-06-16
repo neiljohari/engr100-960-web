12e20cb7892c4c4810cc96a6040ef62c00ae8668
Neil Johari <neil@johari.tech>
2020-06-15 21:46:25 -0400

    Add images to index marketing cards

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,21 +1,25 @@
 function openCard(event, cardId) {
-  card = document.getElementById(cardId);
-    
-  // -> removing the class
-  card.classList.remove("bounce");
-    
-  // -> triggering reflow /* The actual magic */
-  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-  card.offsetWidth = card.offsetWidth;
 
-  // -> and re-adding the class
-  card.classList.add("bounce");
+  card = document.getElementById(cardId);
 
   if(window.innerWidth < 1024) {
     // mobile version
     card.parentElement.querySelectorAll('.card-indicator')[0].classList.toggle('hidden');
   } else {
     // desktop version 
+      
+    // -> removing the class
+    card.classList.remove("bounce");
+      
+    // -> triggering reflow /* The actual magic */
+    // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+    card.offsetWidth = card.offsetWidth;
+
+    // -> and re-adding the class
+    card.classList.add("bounce");
+
+
+    // active selection
     cards = document.querySelectorAll('.marketing-cards > ul .card');
 
     for(let i = 0 ; i < cards.length ; ++i) {
@@ -23,16 +27,25 @@ function openCard(event, cardId) {
     }
 
     peek = document.getElementsByClassName('marketing-peek')[0];
-    peekBody = peek.querySelectorAll('.card > .card-body')[0];
+    peekBody = peek.querySelector('.card > .container > .card-body');
+
+
+    peekImg = peek.querySelector('.card > img');
+
+    console.log(peekImg);
 
     // fade out
     peekBody.style.opacity = 0;
+    peekImg.style.opacity = 0;
 
     // wait for fade out transition
     setTimeout(function(){ 
       // fade in
-      peekBody.innerHTML = card.querySelectorAll(".card-body")[0].innerHTML;
+      peekBody.innerHTML = card.querySelector(".card-body").innerHTML;
       peekBody.style.opacity = 1;
+
+      peekImg.src = card.querySelector('img').src;
+      peekImg.style.opacity = 1;
     },300);
 
   }
