e3121f7d21793799ad395b4195f8d328f0fde6ea
Neil Johari <neil@johari.tech>
2020-07-19 23:15:36 -0400

    Update 2020-07-20T03:15:30.906Z

---
+++ assets/js/staff-min.js
@@ -0,0 +1,1 @@
+function staffSearch(){let e=document.getElementById("staffSearch").value.toUpperCase();function t(t){t.innerHTML.replace(/(<([^>]+)>)/gi," ").toUpperCase().indexOf(e)>-1?t.style.display="":t.style.display="none"}Array.from(document.getElementById("faculty").children).forEach(e=>t(e)),Array.from(document.getElementById("ias").children).forEach(e=>t(e))}
