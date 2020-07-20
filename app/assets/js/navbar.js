2c75b1463bd09b490e65e66c581139694df16137
Neil Johari <neil@johari.tech>
2020-07-19 20:52:05 -0400

    Use localStorage to persist state of navbar open/closed for mobile

--- app/assets/js/navbar.js
+++ app/assets/js/navbar.js
@@ -1,4 +1,18 @@
+window.onload = function () {
+  if (localStorage.getItem("navLinksListOpen")) {
+    var navLinksList = document.getElementById("navLinksList");
+    navLinksList.classList.add("NavLinks_list___active");
+  } else {
+    var navLinksList = document.getElementById("navLinksList");
+    navLinksList.classList.remove("NavLinks_list___active");
+  }
+};
+
 function openNav() {
   var navLinksList = document.getElementById("navLinksList");
-  navLinksList.classList.toggle("NavLinks_list___active");
+  if (navLinksList.classList.toggle("NavLinks_list___active")) {
+    localStorage.setItem("navLinksListOpen", true);
+  } else {
+    localStorage.removeItem("navLinksListOpen");
+  }
 }
--- dist/assets/js/navbar-min.js
+++ dist/assets/js/navbar-min.js
@@ -1,5 +1,16 @@
 function openNav() {
   document
     .getElementById("navLinksList")
-    .classList.toggle("NavLinks_list___active");
+    .classList.toggle("NavLinks_list___active")
+    ? localStorage.setItem("navLinksListOpen", !0)
+    : localStorage.removeItem("navLinksListOpen");
 }
+window.onload = function () {
+  localStorage.getItem("navLinksListOpen")
+    ? document
+        .getElementById("navLinksList")
+        .classList.add("NavLinks_list___active")
+    : document
+        .getElementById("navLinksList")
+        .classList.remove("NavLinks_list___active");
+};
